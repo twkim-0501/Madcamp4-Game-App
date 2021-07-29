211628226a901134ba150dc3b1cfd6b31d267bd5
김찬영 <80759746+ChanyoungKim-kaist@users.noreply.github.com>
2021-07-30 01:05:31 +0900

    moon and button in gamepage

--- madcamp4/src/setupProxy.js
+++ madcamp4/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     proxy({
-      // target: 'http://172.10.18.179:80',
-      target: 'http://172.10.18.171:80',
+      target: 'http://172.10.18.179:80',
+      // target: 'http://172.10.18.171:80',
       changeOrigin: true,
     })
   );
